d9ce6b96bf88415c44b4eae22b6b0d5f43cf61a3
Daiki Mizukami <tesaguriguma@gmail.com>
2019-05-15 17:30:22 +0900

    Support "extended Tweets"

--- src/twitter/models.rs
+++ src/twitter/models.rs
@@ -1,17 +1,13 @@
 use serde::{de, Deserialize};
 
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug)]
 pub struct Tweet {
     pub id: i64,
     pub text: Box<str>,
-    #[serde(default)]
     pub in_reply_to_status_id: Option<i64>,
-    #[serde(default)]
     pub in_reply_to_user_id: Option<i64>,
     pub user: User,
-    #[serde(default)]
     pub quoted_status: Option<QuotedStatus>,
-    #[serde(default)]
     pub retweeted_status: Option<de::IgnoredAny>,
 }
 
@@ -29,4 +25,39 @@ pub struct User {
 #[derive(Clone, Debug, Deserialize)]
 pub struct QuotedStatus {
     pub id: i64,
+}
+
+impl<'de> Deserialize<'de> for Tweet {
+    fn deserialize<D: de::Deserializer<'de>>(d: D) -> Result<Self, D::Error> {
+        #[derive(Deserialize)]
+        struct Prototype {
+            id: i64,
+            text: Box<str>,
+            #[serde(default)]
+            in_reply_to_status_id: Option<i64>,
+            #[serde(default)]
+            in_reply_to_user_id: Option<i64>,
+            user: User,
+            extended_tweet: Option<ExtendedTweet>,
+            #[serde(default)]
+            quoted_status: Option<QuotedStatus>,
+            #[serde(default)]
+            retweeted_status: Option<de::IgnoredAny>,
+        }
+
+        #[derive(Deserialize)]
+        struct ExtendedTweet {
+            full_text: Box<str>,
+        }
+
+        Prototype::deserialize(d).map(|p| Tweet {
+            id: p.id,
+            text: p.extended_tweet.map_or(p.text, |e| e.full_text),
+            in_reply_to_status_id: p.in_reply_to_status_id,
+            in_reply_to_user_id: p.in_reply_to_user_id,
+            user: p.user,
+            quoted_status: p.quoted_status,
+            retweeted_status: p.retweeted_status,
+        })
+    }
 }
