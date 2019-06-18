9c87d488c52f2044ca4782016c67f60f1cffe8da
Daiki Mizukami <tesaguriguma@gmail.com>
2019-06-19 06:28:03 +0900

    Do not wait for a list timeline response before sending next request

--- src/app/twitter_list_timeline.rs
+++ src/app/twitter_list_timeline.rs
@@ -5,11 +5,12 @@ use std::time::{Duration, Instant, SystemTime};
 
 use failure::Fallible;
 
-use futures::compat::Future01CompatExt;
+use futures::compat::Stream01CompatExt;
 use futures::future::FutureExt;
+use futures::stream::StreamExt;
 use futures::{ready, Poll};
 use hyper::client::connect::Connect;
-use tokio::timer::Delay;
+use tokio::timer::Interval;
 
 use crate::twitter::{self, Request as _};
 
@@ -21,8 +22,8 @@ pub struct TwitterListTimeline {
 
 struct Inner {
     list_id: u64,
-    response: Option<twitter::ResponseFuture<Vec<twitter::Tweet>>>,
-    wait: Delay,
+    responses: Vec<twitter::ResponseFuture<Vec<twitter::Tweet>>>,
+    interval: Interval,
     backfill: Option<Backfill>,
 }
 
@@ -30,6 +31,8 @@ struct Backfill {
     since_id: i64,
     response: twitter::ResponseFuture<Vec<twitter::Tweet>>,
 }
+
+const RESP_BUF_CAP: usize = 8;
 
 impl TwitterListTimeline {
     pub fn new<C>(list_id: u64, since_id: Option<i64>, core: &Core<C>) -> Self
@@ -53,8 +56,9 @@ impl TwitterListTimeline {
 
         let mut inner = Inner {
             list_id,
-            response: None,
-            wait: Delay::new(Instant::now()),
+            responses: Vec::with_capacity(RESP_BUF_CAP),
+            // Rate limit of GET lists/statuses (user auth): 900 reqs/15-min window (1 req/sec)
+            interval: Interval::new_interval(Duration::from_secs(1)),
             backfill,
         };
         inner.send_request(core);
@@ -85,24 +89,11 @@ impl TwitterListTimeline {
             return Poll::Ready(Ok(()));
         };
 
-        let response = if let Some(ref mut resp) = inner.response {
-            resp
-        } else {
-            if is_elapsed(&mut inner.wait, cx)? {
-                inner.send_request(core);
-            }
-            return Poll::Pending;
-        };
-
-        let result = ready!(response.poll_unpin(cx));
-
-        if is_elapsed(&mut inner.wait, cx)? {
+        if let Poll::Ready(Some(_)) = (&mut inner.interval).compat().poll_next_unpin(cx) {
             inner.send_request(core);
-        } else {
-            inner.response = None;
         }
 
-        let tweets = match result {
+        let tweets = match ready!(inner.poll_next(cx)) {
             Ok(resp) => resp.response,
             Err(e) => {
                 // Skip the error as the list timeline is only for complementary purpose.
@@ -115,7 +106,7 @@ impl TwitterListTimeline {
                             let eta = reset
                                 .duration_since(now_s)
                                 .unwrap_or(Duration::from_secs(0));
-                            inner.wait.reset(now_i + eta);
+                            inner.interval = Interval::new(now_i + eta, Duration::from_secs(1));
                         }
                     }
                 }
@@ -214,6 +205,26 @@ impl TwitterListTimeline {
 }
 
 impl Inner {
+    fn poll_next(
+        &mut self,
+        cx: &mut Context<'_>,
+    ) -> Poll<twitter::Result<twitter::Response<Vec<twitter::Tweet>>>> {
+        // Iterate from later responses.
+        for (i, resp) in self.responses.iter_mut().enumerate().rev() {
+            if let Poll::Ready(result) = resp.poll_unpin(cx) {
+                // Discard earlier pending responses as well since we have newer data now.
+                if i > 0 {
+                    debug!("dropping {} pending list response(s)", i);
+                }
+                self.responses.drain(0..=i);
+
+                return Poll::Ready(result);
+            }
+        }
+
+        Poll::Pending
+    }
+
     fn send_request<C>(&mut self, core: &Core<C>)
     where
         C: Connect + Sync + 'static,
@@ -222,8 +233,6 @@ impl Inner {
     {
         trace_fn!(Inner::send_request::<C>);
 
-        // Rate limit of GET lists/statuses (user auth): 900 reqs/15-min window (1 req/sec)
-        self.wait.reset(Instant::now() + Duration::from_secs(1));
         let resp = twitter::lists::Statuses::new(self.list_id)
             .count(None)
             .include_rts(Some(false))
@@ -232,10 +241,12 @@ impl Inner {
                 core.twitter_token(core.manifest().twitter.user).unwrap(),
                 core.http_client(),
             );
-        self.response = Some(resp);
-    }
-}
 
-fn is_elapsed(delay: &mut Delay, cx: &mut Context<'_>) -> Fallible<bool> {
-    Ok(delay.is_elapsed() || delay.compat().poll_unpin(cx)?.is_ready())
+        if self.responses.len() == self.responses.capacity() {
+            debug!("respone buffer reached its capacity");
+            self.responses.remove(0);
+        }
+
+        self.responses.push(resp);
+    }
 }
