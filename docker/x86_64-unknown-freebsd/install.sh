d4a08fc047702944f2b0ca5045328de3a195a4f2
Daiki Mizukami <tesaguriguma@gmail.com>
2020-08-07 14:32:15 +0900

    Fix

--- docker/x86_64-unknown-freebsd/install.sh
+++ docker/x86_64-unknown-freebsd/install.sh
@@ -1,14 +1,43 @@
+#!/usr/bin/env bash
+
 set -ex
 
-temp="$(mktemp -d)"
-pushd "$temp"
+main() {
+    local arch="$1"
 
-curl -sSfLO 'https://sqlite.org/2020/sqlite-autoconf-3320300.tar.gz'
-tar -xzf 'sqlite-autoconf-3320300.tar.gz'
-cd 'sqlite-autoconf-3320300'
-./configure --prefix='/usr/local/x86_64-unknown-freebsd10'
-make install "-j$(nproc)"
+    local dependencies=(
+        ca-certificates
+        curl
+        gzip
+        make
+    )
+    local purge_list=()
 
-popd
-rm -rf "$temp"
-rm "$0"
+    apt-get update
+    for dep in "${dependencies[@]}"; do
+        if ! dpkg -L "$dep"; then
+            apt-get install --no-install-recommends --assume-yes "$dep"
+            purge_list+=( "$dep" )
+        fi
+    done
+
+    temp="$(mktemp -d)"
+    pushd "$temp"
+
+    curl -sSfLO 'https://sqlite.org/2020/sqlite-autoconf-3320300.tar.gz'
+    tar -xzf 'sqlite-autoconf-3320300.tar.gz'
+    cd 'sqlite-autoconf-3320300'
+    ./configure --prefix="/usr/local/$arch-unknown-freebsd10"
+    make install "-j$(nproc)"
+
+    popd
+
+    if (( ${#purge_list[@]} )); then
+        apt-get purge --assume-yes --auto-remove "${purge_list[@]}"
+    fi
+
+    rm -rf "$temp"
+    rm "$0"
+}
+
+main "$@"
