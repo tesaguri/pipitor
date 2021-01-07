859b1b1b01c9d5542321029af71e465903246d20
Daiki Mizukami <tesaguriguma@gmail.com>
2021-01-08 04:42:33 +0900

    Use HTTP/2 with `native-tls`

--- src/bin/pipitor/common.rs
+++ src/bin/pipitor/common.rs
@@ -122,7 +122,13 @@ cfg_if::cfg_if! {
         use hyper_tls::HttpsConnector;
 
         pub fn https_connector() -> HttpsConnector<HttpConnector> {
-            HttpsConnector::new()
+            let mut h = HttpConnector::new();
+            h.enforce_http(false);
+            let c = native_tls_pkg::TlsConnector::builder()
+                .request_alpns(&["h2"])
+                .build()
+                .unwrap();
+            HttpsConnector::from((h, c.into()))
         }
     } else {
         compile_error!("Either `native-tls` or `rustls` feature is required");
