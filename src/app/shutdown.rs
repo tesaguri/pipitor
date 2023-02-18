5efebecd0baf7b3eb35f34cc190cb61d85016df3
Daiki Mizukami <tesaguriguma@gmail.com>
2023-02-18 22:37:21 +0900

    Fix hang-up of `Shutdown`

--- src/app/shutdown.rs
+++ src/app/shutdown.rs
@@ -81,21 +81,56 @@ impl<F: Future> Future for HandleFuture<F> {
 
 #[cfg(test)]
 mod tests {
+    use std::time::Duration;
+
     use super::*;
 
     #[test]
-    fn test() {
+    fn no_extra_wakeup() {
         let shutdown = Shutdown::default();
         let handle = shutdown.handle.clone();
         let mut task = tokio_test::task::spawn(shutdown);
 
         task.enter(|cx, shutdown| assert!(shutdown.poll(cx).is_pending()));
+        assert!(!task.is_woken());
 
         let cloned = handle.clone();
+        assert!(!task.is_woken());
+
         drop(handle);
+        assert!(!task.is_woken());
+
         task.enter(|cx, shutdown| assert!(shutdown.poll(cx).is_pending()));
+        assert!(!task.is_woken());
 
         drop(cloned);
+        assert!(task.is_woken());
+
+        // This is not sufficient for testing the absence of hang-ups, since we need to check that
+        // the wake-up occurs after the decrement of the strong count, both of which occurs during
+        // the drop. That will be tested in the subsequent `no_hang_up` test.
         task.enter(|cx, shutdown| assert!(shutdown.poll(cx).is_ready()));
+    }
+
+    #[tokio::test]
+    async fn no_hang_up() {
+        let mut shutdown = Shutdown::default();
+        let handle = shutdown.handle.clone();
+
+        let shutdown_task = tokio::spawn(async move {
+            let timeout = tokio::time::sleep(Duration::from_secs(1));
+            tokio::pin!(timeout);
+            loop {
+                tokio::select! {
+                    biased;
+                    () = &mut timeout => panic!("timed out"),
+                    () = &mut shutdown => break,
+                }
+            }
+        });
+
+        let handle_task =
+            tokio::spawn(handle.wrap_future(tokio::time::sleep(Duration::from_micros(1))));
+        tokio::try_join!(shutdown_task, handle_task).unwrap();
     }
 }
